8a79b6c802277efc976230cd8a5c9c79b0c88a50
Petr Krysl <pkrysl@ucsd.edu>
2024-05-20 19:10:04 -0700

    add initial sources

---
+++ src/FinEtoolsDomDecomp.jl
@@ -0,0 +1,10 @@
+module FinEtoolsDomDecomp
+
+# Enable LSP look up in test modules
+if false
+    include("../test/runtests.jl")
+end
+
+using FinEtools
+
+end # module FinEtoolsDomDecomp
---
+++ test/test_partitioning.jl
@@ -0,0 +1,19 @@
+module mbas001
+using Test
+using FinEtools
+using FinEtools.MeshExportModule: VTK
+using Metis
+function test()
+    ndoms = 3
+    fens, fes = T3block(1.0, 1.0, 10, 13)
+    femm1 = FEMMBase(IntegDomain(fes, SimplexRule(3, 1)))
+    C = dualconnectionmatrix(femm1, fens, 2)
+    g = Metis.graph(C; check_hermitian=true)
+    partitioning = Metis.partition(g, ndoms; alg=:KWAY)
+    VTK.vtkexportmesh("mt014.vtk", connasarray(fes), fens.xyz, VTK.T3; scalars=[("partitioning", partitioning)])
+    true
+end
+test()
+nothing
+end
+
