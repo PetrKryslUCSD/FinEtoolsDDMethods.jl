14821457ae2dc33d769aac23574bed84adcf4d9b
Petr Krysl <pkrysl@ucsd.edu>
2024-06-12 17:25:56 -0700

    fix mismatched numbers of partitions

--- examples/heat/conc/Poisson2D_cg.jl
+++ examples/heat/conc/Poisson2D_cg.jl
@@ -31,8 +31,8 @@ function test()
     thermal_conductivity = [i == j ? one(Float64) : zero(Float64) for i = 1:2, j = 1:2] # conductivity matrix
     Q = -6.0 # internal heat generation rate
     
-    npartitions = 80
-    nbf1max = 6
+    npartitions = 44
+    nbf1max = 3
 
     tempf(x) = (1.0 .+ x[:, 1] .^ 2 .+ 2 * x[:, 2] .^ 2)#the exact distribution of temperature
     N = 1000 # number of subdivisions along the sides of the square domain
@@ -71,6 +71,7 @@ function test()
     F2 = - K_fd * T_d
     
     partitioning = nodepartitioning(fens, npartitions)
+    partitionnumbers = unique(partitioning)
     mor = CoNCData(fens, partitioning)
     Phi = transfmatrix(mor, LegendreBasis, nbf1max, Temp)
     Phi = Phi[fr, :]
@@ -84,7 +85,7 @@ function test()
     # Ur = Phi * (Krfactor \ (PhiT * F))
 
     partitions = []
-    for i in 1:npartitions
+    for i in partitionnumbers
         pnl = findall(x -> x == i, partitioning)
         doflist = [Temp.dofnums[n] for n in pnl if Temp.dofnums[n] <= nfreedofs(Temp)]
         pK = K[doflist, doflist]
@@ -93,32 +94,26 @@ function test()
         push!(partitions, part)
     end
 
+    @show length(partitions)
+
     function M!(q, p)
         q .= Phi * (Krfactor \ (PhiT * p))
+        # rp = p - Phi * (PhiT * p)
         # q .= 0.0
         for part in partitions
-            q[part.doflist] .+= part.factor \ p[part.doflist]
+            q[part.doflist] .+= (part.factor \ p[part.doflist])
         end
         q
     end
 
-    (T_f, stats) = pcg_seq((q, p) -> mul!(q, K_ff, p), F1 + F2, zeros(size(F1)); 
-        M! = (q, p) -> M!(q, p), 
+    (T_f, stats) = pcg_seq((q, p) -> mul!(q, K_ff, p), F1 + F2, zeros(size(F1));
+        (M!)=(q, p) -> M!(q, p),
         itmax=1000, atol=1e-10, rtol=1e-10)
-        @show stats
-        scattersysvec!(Temp, T_f)
-
-    # fr = dofrange(Temp, DOF_KIND_FREE)
-    # dr = dofrange(Temp, DOF_KIND_DATA)
-    # K_ff = K[fr, fr]
-    # K_fd = K[fr, dr]
-    # F_f = F1[fr]
-    # T_d = gathersysvec(Temp, DOF_KIND_DATA)
-    # @time T_f = K_ff \ (F_f - K_fd * T_d)
-    # scattersysvec!(Temp, T_f)
+    @show stats
+    scattersysvec!(Temp, T_f)
     
     T = deepcopy(Temp.values)
-    VTK.vtkexportmesh("ref.vtk", fes.conn, [geom.values T], VTK.T3; scalars=[("Temperature", T,)])
+    VTK.vtkexportmesh("Poisson2D_cg-approx.vtk", fes.conn, [geom.values T], VTK.T3; scalars=[("Temperature", T,)])
 
 
     Error = 0.0
