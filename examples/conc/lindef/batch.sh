095af0404d99fe187ead5b0d8acdc32946b02193
Petr Krysl <pkrysl@ucsd.edu>
2024-06-22 09:44:49 -0700

    update batch

--- examples/conc/lindef/batch.sh
+++ examples/conc/lindef/batch.sh
@@ -1,5 +1,5 @@
 for rf in 4 6; do
-    for nf in 4 6; do
+    for nf in 4 8 16; do
         for n1 in $(seq 2 5); do
             for ne in $(seq 400 400 2000); do
                 julia conc/lindef/cc.jl --nelperpart $ne --nbf1max $n1 --ref $rf --nfpartitions $nf; 
