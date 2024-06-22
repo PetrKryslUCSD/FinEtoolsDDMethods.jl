32a04851bf81974efcd4dce93e00913171a45325
Petr Krysl <pkrysl@ucsd.edu>
2024-06-22 08:10:23 -0700

    adjust parametric study ranges

--- examples/conc/lindef/batch.sh
+++ examples/conc/lindef/batch.sh
@@ -1,7 +1,7 @@
 for rf in 4 6; do
     for nf in 4 6; do
         for n1 in $(seq 2 5); do
-            for ne in $(seq 800 200 1000); do
+            for ne in $(seq 400 400 2000); do
                 julia conc/lindef/cc.jl --nelperpart $ne --nbf1max $n1 --ref $rf --nfpartitions $nf; 
             done 
         done
