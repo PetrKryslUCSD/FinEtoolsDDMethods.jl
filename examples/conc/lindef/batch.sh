87fa3c67e9075b4d36d44c5b55cdf873b9fcb098
Petr Krysl <pkrysl@ucsd.edu>
2024-06-22 10:16:32 -0700

    take into account the possibility that the results are already there

--- examples/conc/lindef/batch.sh
+++ examples/conc/lindef/batch.sh
@@ -1,8 +1,14 @@
+script=cc
 for rf in 4 6; do
     for nf in 4 8 16; do
         for n1 in $(seq 2 5); do
             for ne in $(seq 400 400 2000); do
-                julia conc/lindef/cc.jl --nelperpart $ne --nbf1max $n1 --ref $rf --nfpartitions $nf; 
+                jsonfile=fibers-${script}-hex-rf=$rf-ne=$ne-n1=$n1-nf=$nf.json
+                if [ -f ${jsonfile} ]; then
+                    echo "${jsonfile} exists" 
+                else
+                    julia conc/lindef/${script}.jl --nelperpart $ne --nbf1max $n1 --ref $rf --nfpartitions $nf; 
+                fi
             done 
         done
     done
