17863b3771f907d9e3c51a5ee5e6a427c5589927
Petr Krysl <pkrysl@ucsd.edu>
2024-07-03 16:34:38 -0700

    add overlapping solver for the shell

---
+++ examples/conc/shells/cos_2t_press_hyperboloid_free_overlapped_examples.jl
@@ -0,0 +1,311 @@
+module cos_2t_press_hyperboloid_free_overlapped_examples
+using FinEtools
+using FinEtools.MeshExportModule: VTK
+using FinEtoolsDeforLinear
+using FinEtoolsFlexStructures.FESetShellT3Module: FESetShellT3
+using FinEtoolsFlexStructures.FESetShellQ4Module: FESetShellQ4
+using FinEtoolsFlexStructures.FEMMShellT3FFModule
+using FinEtoolsFlexStructures.RotUtilModule: initial_Rfield, update_rotation_field!
+using FinEtoolsDDParallel
+using FinEtoolsDDParallel.CGModule: pcg_seq
+using SymRCM
+using Metis
+using Test
+using LinearAlgebra
+using SparseArrays
+using PlotlyLight
+using Krylov
+using LinearOperators
+import Base: size, eltype
+import LinearAlgebra: mul!
+import CoNCMOR: CoNCData, transfmatrix, LegendreBasis
+using Targe2
+using DataDrop
+using Statistics
+
+
+function coarse_grid_partitioning(fens, fes, nelperpart)
+    partitioning = zeros(Int, count(fens))
+    nfiberparts = 0
+    for p in 1:maximum(fes.label)
+        fiberel = selectelem(fens, fes, label = p)
+        femm = FEMMBase(IntegDomain(subset(fes, fiberel), PointRule()))
+        C = dualconnectionmatrix(femm, fens, nodesperelem(boundaryfe(fes)))
+        g = Metis.graph(C; check_hermitian=true)
+        ndoms = Int(ceil(length(fiberel) / nelperpart))
+        fiber_element_partitioning = Metis.partition(g, ndoms; alg=:KWAY)
+        for e in eachindex(fiberel)
+            for n in fes.conn[fiberel[e]]
+                partitioning[n] = fiber_element_partitioning[e] + nfiberparts
+            end
+        end
+        nfiberparts += maximum(fiber_element_partitioning)
+    end
+    matrixel = selectelem(fens, fes, label = 0)
+    femm = FEMMBase(IntegDomain(subset(fes, matrixel), PointRule()))
+    C = dualconnectionmatrix(femm, fens, nodesperelem(boundaryfe(fes)))
+    g = Metis.graph(C; check_hermitian=true)
+    ndoms = Int(ceil(length(matrixel) / nelperpart))
+    matrix_element_partitioning = Metis.partition(g, ndoms; alg=:KWAY)
+    for e in eachindex(matrixel)
+        for n in fes.conn[matrixel[e]]
+            if partitioning[n] == 0
+                partitioning[n] = matrix_element_partitioning[e] + nfiberparts
+            end
+        end
+    end
+    npartitions = maximum(partitioning)
+    partitioning, npartitions
+end
+
+function make_overlapping_partition(fens, fes, n2e, overlap, element_1st_partitioning, i)
+    enl = findall(x -> x == i, element_1st_partitioning)
+    touched = fill(false, count(fes)) 
+    touched[enl] .= true 
+    for ov in 1:overlap
+        sfes = subset(fes, enl)
+        bsfes = meshboundary(sfes)
+        addenl = Int[]
+        for e in eachindex(bsfes)
+            for n in bsfes.conn[e]
+                for ne in n2e.map[n]
+                    if !touched[ne] 
+                        touched[ne] = true
+                        push!(addenl, ne)
+                    end
+                end
+            end
+        end
+        enl = cat(enl, addenl; dims=1)
+    end
+    # vtkexportmesh("i=$i" * "-enl" * "-final" * ".vtk", fens, subset(fes, enl))
+    return connectednodes(subset(fes, enl))
+end
+
+function fine_grid_node_lists(fens, fes, npartitions, overlap)
+    femm = FEMMBase(IntegDomain(fes, PointRule()))
+    C = dualconnectionmatrix(femm, fens, nodesperelem(boundaryfe(fes)))
+    g = Metis.graph(C; check_hermitian=true)
+    element_1st_partitioning = Metis.partition(g, npartitions; alg=:KWAY)
+    npartitions = maximum(element_1st_partitioning)
+    n2e = FENodeToFEMap(fes, count(fens))
+    nodelists = []
+    for i in 1:npartitions
+        push!(nodelists, make_overlapping_partition(fens, fes, n2e, overlap, element_1st_partitioning, i))
+    end
+    nodelists
+end
+
+# Parameters:
+E = 2.0e11
+nu = 1/3;
+pressure = 1.0e3;
+Length = 2.0;
+
+function computetrac!(forceout, XYZ, tangents, feid, qpid)
+    r = vec(XYZ); r[2] = 0.0
+    r .= vec(r)/norm(vec(r))
+    theta = atan(r[3], r[1])
+    n = cross(tangents[:, 1], tangents[:, 2]) 
+    n = n/norm(n)
+    forceout[1:3] = n*pressure*cos(2*theta)
+    forceout[4:6] .= 0.0
+    # @show dot(n, forceout[1:3])
+    return forceout
+end
+
+function _execute(ncoarse, thickness, nelperpart, nbf1max, nfpartitions, overlap, ref, itmax)
+    CTE = 0.0
+    distortion = 0.0
+    n = ncoarse  * ref    # number of elements along the edge of the block
+    tolerance = Length / n/ 100
+    
+    fens, fes = distortblock(T3block, 90/360*2*pi, Length/2, n, n, distortion, distortion);
+    fens.xyz = xyz3(fens)
+    for i in 1:count(fens)
+        a=fens.xyz[i, 1]; y=fens.xyz[i, 2];
+        R = sqrt(1 + y^2)
+        fens.xyz[i, :] .= (R*sin(a), y, R*cos(a))
+    end
+    
+    MR = DeforModelRed3D
+    mater = MatDeforElastIso(MR, 0.0, E, nu, CTE)
+
+    sfes = FESetShellT3()
+    accepttodelegate(fes, sfes)
+    femm = FEMMShellT3FFModule.make(IntegDomain(fes, TriRule(1), thickness), mater)
+    stiffness = FEMMShellT3FFModule.stiffness
+    associategeometry! = FEMMShellT3FFModule.associategeometry!
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    # plane of symmetry perpendicular to Z
+    l1 = selectnode(fens; box = Float64[-Inf Inf -Inf Inf 0 0], inflate = tolerance)
+    for i in [3,4,5]
+        setebc!(dchi, l1, true, i)
+    end
+    # plane of symmetry perpendicular to Y
+    l1 = selectnode(fens; box = Float64[-Inf Inf 0 0 -Inf Inf], inflate = tolerance)
+    for i in [2,4,6]
+        setebc!(dchi, l1, true, i)
+    end
+    # plane of symmetry perpendicular to X
+    l1 = selectnode(fens; box = Float64[0 0 -Inf Inf -Inf Inf], inflate = tolerance)
+    for i in [1,5,6]
+        setebc!(dchi, l1, true, i)
+    end
+    # clamped edge perpendicular to Y
+    # l1 = selectnode(fens; box = Float64[-Inf Inf L/2 L/2 -Inf Inf], inflate = tolerance)
+    # for i in [1,2,3,4,5,6]
+    #     setebc!(dchi, l1, true, i)
+    # end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    fr = dofrange(dchi, DOF_KIND_FREE)
+    dr = dofrange(dchi, DOF_KIND_DATA)
+    
+    println("Refinement factor: $(ref)")
+    println("Number of elements per partition: $(nelperpart)")
+    println("Number of 1D basis functions: $(nbf1max)")
+    println("Number of fine grid partitions: $(nfpartitions)")
+    println("Overlap: $(overlap)")
+    println("Number of elements: $(count(fes))")
+    println("Number of free dofs = $(nfreedofs(dchi))")
+
+    lfemm = FEMMBase(IntegDomain(fes, TriRule(3)))
+    fi = ForceIntensity(Float64, 6, computetrac!);
+    F = distribloads(lfemm, geom0, dchi, fi, 2);
+    F_f = F[fr]
+
+    associategeometry!(femm, geom0)
+    K = stiffness(femm, geom0, u0, Rfield0, dchi);
+    K_ff = K[fr, fr]
+
+    # U_f = K_ff \ F_f
+    # scattersysvec!(u, U_f)
+
+    # VTK.vtkexportmesh("fibers-tet-sol.vtk", fens, fes; vectors=[("u", deepcopy(u.values),)])   
+
+    cpartitioning, ncpartitions = coarse_grid_partitioning(fens, fes, nelperpart)
+    println("Number coarse grid partitions: $(ncpartitions)")
+        
+    # f = "fibers_soft_hard_$(string(kind))" *
+    #     "-rf=$(ref)" *
+    #     "-ne=$(nelperpart)" *
+    #     "-n1=$(nbf1max)" * "-partitioning"
+    # partitionsfes = FESetP1(reshape(1:count(fens), count(fens), 1))
+    # vtkexportmesh(f * ".vtk", fens, partitionsfes; scalars=[("partition", cpartitioning)])
+    # @async run(`"paraview.exe" $File`)
+    
+    mor = CoNCData(fens, cpartitioning)
+    Phi = transfmatrix(mor, LegendreBasis, nbf1max, dchi)
+    Phi = Phi[fr, :]
+    transfm(m, t, tT) = (tT * m * t)
+    transfv(v, t, tT) = (tT * v)
+    PhiT = Phi'
+    Kr_ff = transfm(K_ff, Phi, PhiT)
+    println("Size of the reduced problem: $(size(Kr_ff))")
+    Krfactor = lu(Kr_ff)
+
+    # U_f = Phi * (Krfactor \ (PhiT * F_f))
+    # scattersysvec!(u, U_f)
+
+    # VTK.vtkexportmesh("fibers-tet-red-sol.vtk", fens, fes; vectors=[("u", deepcopy(u.values),)])   
+    
+    # fpartitioning, nfpartitions = fine_grid_partitioning(fens, nfpartitions)
+    nodelists = fine_grid_node_lists(fens, fes, nfpartitions, overlap)
+    @assert length(nodelists) == nfpartitions
+    
+    partitions = []
+    for nodelist in nodelists
+        doflist = Int[]
+        for n in nodelist
+            for d in axes(dchi.dofnums, 2)
+                if dchi.dofnums[n, d] in fr
+                    push!(doflist, dchi.dofnums[n, d])
+                end
+            end
+        end
+        pK = K[doflist, doflist]
+        pKfactor = lu(pK)
+        part = (nodelist = nodelist, factor = pKfactor, doflist = doflist)
+        push!(partitions, part)
+    end
+
+    meansize = mean([length(part.doflist) for part in partitions])
+    println("Mean fine partition size: $(meansize)")
+
+    function M!(q, p)
+        q .= Phi * (Krfactor \ (PhiT * p))
+        for part in partitions
+            q[part.doflist] .+= (part.factor \ p[part.doflist])
+        end
+        q
+    end
+
+    t0 = time()
+    norm_F_f = norm(F_f)
+    (u_f, stats) = pcg_seq((q, p) -> mul!(q, K_ff, p), F_f, zeros(size(F_f));
+        (M!)=(q, p) -> M!(q, p),
+        itmax=itmax, atol=1e-6 * norm_F_f, rtol=0)
+    t1 = time()
+    println("Number of iterations:  $(stats.niter)")
+    stats = (niter = stats.niter, residuals = stats.residuals ./ norm_F_f)
+    data = Dict(
+        "nfreedofs_dchi" => nfreedofs(dchi),
+        "ncpartitions" => ncpartitions,
+        "nfpartitions" => nfpartitions,
+        "overlap" => overlap,
+        "size_Kr_ff" => size(Kr_ff),
+        "stats" => stats,
+        "time" => t1 - t0,
+    )
+    f = "cos_2t_press_hyperboloid_free_overlapped" *
+        "-rf=$(ref)" *
+        "-ne=$(nelperpart)" *
+        "-n1=$(nbf1max)"  * 
+        "-nf=$(nfpartitions)"  * 
+        "-ov=$(overlap)"  
+    DataDrop.store_json(f * ".json", data)
+    scattersysvec!(dchi, u_f)
+    
+    # f = "cos_2t_press_hyperboloid_free_overlapped" *
+    #     "-rf=$(ref)" *
+    #     "-ne=$(nelperpart)" *
+    #     "-n1=$(nbf1max)" * 
+    #     "-nf=$(nfpartitions)"  * 
+    #     "-cg-sol"
+    # VTK.vtkexportmesh(f * ".vtk", fens, fes; vectors=[("u", deepcopy(u.values),)])
+    VTK.vtkexportmesh("cos_2t_press_hyperboloid_free-sol.vtk", fens, fes; 
+    vectors=[("u", deepcopy(dchi.values[:, 1:3]),)])
+    
+    p = 1
+    for nodelist in nodelists
+        cel = connectedelems(fes, nodelist, count(fens))
+        vtkexportmesh("cos_2t_press_hyperboloid_free-patch$(p).vtk", fens, subset(fes, cel))
+        sfes = FESetP1(reshape(nodelist, length(nodelist), 1))
+        vtkexportmesh("cos_2t_press_hyperboloid_free-nodes$(p).vtk", fens, sfes)
+        p += 1
+    end
+
+    true
+end
+# test(ref = 1)
+
+function test(nelperpart = 100, nbf1max = 5, nfpartitions = 2, overlap = 1, ref = 1)
+    thickness = Length/2/100
+    itmax = 2000
+    _execute(16, thickness, nelperpart, nbf1max, nfpartitions, overlap, ref, itmax)
+end
+
+nothing
+end # module
+
+
