bdeca8a589a9aa6c26761a4e9e8ff01830babaca
Petr Krysl <pkrysl@ucsd.edu>
2024-07-24 07:48:37 -0700

    add a shell script for the barrel

---
+++ examples/conc/shells/barrel.sh
@@ -0,0 +1,18 @@
+script=barrel
+for rf in 1; do
+    for nf in 8 16 32; do
+        for n1 in 4 5 6; do
+            for ne in $(seq 1000 2000 4000); do
+                for ov in 1 3 5; do
+                    jsonfile=barrel_overlapped-rf=$rf-ne=$ne-n1=$n1-nf=$nf-ov=$ov.json
+                    if [ -f ${jsonfile} ]; then
+                        echo "${jsonfile} exists" 
+                    else
+                        julia conc/shells/${script}.jl --nelperpart $ne --nbf1max $n1 --ref $rf --nfpartitions $nf --overlap $ov ; 
+                    fi
+                done
+            done 
+        done
+    done
+done
+
