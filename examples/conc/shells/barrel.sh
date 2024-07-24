96191b98f572b6ceb3d6e92c4a779ae794fcbb4e
Petr Krysl <pkrysl@ucsd.edu>
2024-07-24 08:34:54 -0700

    change the range

--- examples/conc/shells/barrel.sh
+++ examples/conc/shells/barrel.sh
@@ -2,7 +2,7 @@ script=barrel
 for rf in 1; do
     for nf in 8 16 32; do
         for n1 in 4 5 6; do
-            for ne in $(seq 1000 2000 4000); do
+            for ne in $(seq 200 400 1000); do
                 for ov in 1 3 5; do
                     jsonfile=barrel_overlapped-rf=$rf-ne=$ne-n1=$n1-nf=$nf-ov=$ov.json
                     if [ -f ${jsonfile} ]; then
