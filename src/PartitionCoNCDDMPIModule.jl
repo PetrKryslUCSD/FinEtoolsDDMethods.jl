3bbed8f054570d9a905c63494f4bf6e628c5b6a6
Petr Krysl <pkrysl@ucsd.edu>
2024-08-18 12:56:50 -0700

    update mpi solve

--- src/PartitionCoNCDDMPIModule.jl
+++ src/PartitionCoNCDDMPIModule.jl
@@ -177,28 +177,29 @@ function CoNCPartitionData(cpi::CPI, i, fes, Phi, make_matrix) where {CPI<:CoNCP
     return CoNCPartitionData(Kn_ff, Kr_ff, lu(Ko), ndof, ntempq, ntempp, odof, otempq, otempp)
 end
 
-function partition_multiply!(q, partitions, p)
+function partition_multiply!(q, partition, p)
     q .= zero(eltype(q))
-    for _p in partitions
-        d = _p.ndof
-        _p.ntempp .= p[d]
-        mul!(_p.ntempq, _p.nonoverlapping_K, _p.ntempp)
-    end
-    for _p in partitions
-        d = _p.ndof
-        q[d] .+= _p.ntempq
+    if partition !== nothing
+        d = partition.ndof
+        partition.ntempp .= p[d]
+        mul!(partition.ntempq, partition.nonoverlapping_K, partition.ntempp)
+        q[d] .+= partition.ntempq
     end
     q
 end
 
-function precondition_solve!(q, Krfactor, Phi, partitions, p) 
+function precondition_global_solve!(q, Krfactor, Phi, p) 
     q .= Phi * (Krfactor \ (Phi' * p))
-    for _p in partitions
-        d = _p.odof
-        _p.otempp .= p[d]
-        ldiv!(_p.otempq, _p.overlapping_K_factor, _p.otempp)
-        q[d] .+= _p.otempq
-        # q[d] .+= (cp.pdata[i].overlapping_factor \ p[d])
+    q
+end
+
+function precondition_local_solve!(q, partition, p) 
+    q .= zero(eltype(q))
+    if partition !== nothing
+        d = partition.odof
+        partition.otempp .= p[d]
+        ldiv!(partition.otempq, partition.overlapping_K_factor, partition.otempp)
+        q[d] .+= partition.otempq
     end
     q
 end
