0bcd18eb390a4787260f5f24aab14ed12a98fe98
Petr Krysl <pkrysl@ucsd.edu>
2024-08-25 19:27:41 -0700

    fix file naming

--- examples/conc/lindef/cc.sh
+++ examples/conc/lindef/cc.sh
@@ -4,7 +4,7 @@ for rf in 6 8; do
         for n1 in $(seq 2 2 6); do
             for ne in $(seq 400 800 2000); do
                 for ov in 1 3 5; do
-                    jsonfile=fibers-${script}-hex-rf=$rf-ne=$ne-n1=$n1-nf=$nf.json
+                    jsonfile=fibers-${script}-hex-rf=$rf-ne=$ne-n1=$n1-nf=$nf-ov=$ov.json
                     if [ -f ${jsonfile} ]; then
                         echo "${jsonfile} exists" 
                     else
