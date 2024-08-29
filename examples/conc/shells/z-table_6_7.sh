39768ac9004a42d32f5c19cbc9e92c8f8df082de
Petr Krysl <pkrysl@ucsd.edu>
2024-08-28 17:35:49 -0700

    add z

---
+++ examples/conc/shells/z-table_6_7.sh
@@ -0,0 +1,14 @@
+script=z
+for as in 10; do
+    for rf in  8; do
+        for nf in 32 64 128 256 512; do
+            for n1 in 4 5 6; do
+                for ne in  20000 200000 ; do
+                    for ov in 5 ; do
+                        julia conc/shells/${script}.jl --nelperpart $ne --nbf1max $n1 --ref $rf --nfpartitions $nf --overlap $ov --aspect $as; 
+                    done
+                done 
+            done
+        done
+    done
+done
