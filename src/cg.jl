7c66590db36352460cba4f1a387beceefc14dd11
Petr Krysl <pkrysl@ucsd.edu>
2024-08-29 14:32:19 -0700

    clean up

--- src/cg.jl
+++ src/cg.jl
@@ -94,51 +94,6 @@ function pcg_seq(Aop!, b, x0;
     return (x, stats)
 end
 
-# Smith, Gropp  1996
-# There is something weird about this: it takes quite a few more iterations!
-function pcg_seq_sg(Aop!, b, x0;
-    (M!)=(q, p) -> (q .= p),
-    itmax=0,
-    atol=√eps(eltype(b)),
-    rtol=√eps(eltype(b)),
-    peeksolution=(iter, x, resnorm) -> nothing
-    )
-    itmax = (itmax > 0 ? itmax : length(b))
-    x = deepcopy(x0); p = similar(x); r = similar(x); z = similar(x); 
-    Ap = z # Alias for legibility
-    tol = zero(typeof(atol))
-    resnorm = Inf
-    residuals = typeof(tol)[]
-    stats = (niter=itmax, resnorm=resnorm, residuals=residuals)
-    betaold = one(typeof(atol))
-    Aop!(Ap, x) 
-    @. r = b - Ap
-    M!(z, r)
-    @. p = z
-    betaold = dot(z, r)
-    iter = 1
-    while iter < itmax
-        beta = dot(z, r)
-        c = beta / betaold; betaold = beta
-        @. p = z + c * p
-        Aop!(Ap, p)
-        @show a = beta / dot(p, Ap)
-        @. x += a * p
-        @. r -= a * Ap
-        resnorm = sqrt(dot(r, r))
-        push!(residuals, resnorm)
-        peeksolution(iter, x, resnorm)
-        tol == 0 && (tol = atol + rtol * sqrt(beta))
-        if resnorm < tol
-            break
-        end
-        M!(z, r)
-        iter += 1
-    end
-    stats = (niter=iter, resnorm=resnorm, residuals=residuals)
-    return (x, stats)
-end
-
 """
     pcg_mpi_2level_Schwarz(
         comm, 
