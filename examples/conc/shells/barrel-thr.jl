1758437d0242cedf36dca792d2ea8bcbe8ea9d00
Petr Krysl <pkrysl@ucsd.edu>
2024-08-30 13:05:10 -0700

    add driver for threaded testing

---
+++ examples/conc/shells/barrel-thr.jl
@@ -0,0 +1,65 @@
+println("Current folder: $(pwd())")
+
+using Pkg
+
+Pkg.activate(".")
+Pkg.instantiate()
+
+using ArgParse
+
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table! s begin
+        "--nelperpart"
+        help = "Number of elements per partition"
+        arg_type = Int
+        default = 1200
+        "--nbf1max"
+        help = "Number 1D basis functions"
+        arg_type = Int
+        default = 5
+        "--nfpartitions"
+        help = "Number fine grid partitions"
+        arg_type = Int
+        default = 16
+        "--overlap"
+        help = "Overlap"
+        arg_type = Int
+        default = 1
+        "--ref"
+        help = "Refinement factor"
+        arg_type = Int
+        default = 2
+        "--itmax"
+        help = "Maximum number of iterations allowed"
+        arg_type = Int
+        default = 2000
+        "--relrestol"
+        help = "Relative residual tolerance"
+        arg_type = Float64
+        default = 1.0e-6
+        "--stabilize"
+        help = "Should the axial rotation be stabilized? factor"
+        arg_type = Bool
+        default = false
+        "--visualize"
+        help = "Write out visualization files?"
+        arg_type = Bool
+        default = false
+    end
+    return parse_args(s)
+end
+
+p = parse_commandline()
+
+include(raw"barrel_examples.jl")
+using .barrel_examples; 
+
+
+barrel_examples.test(;
+    nelperpart=p["nelperpart"], nbf1max=p["nbf1max"],
+    nfpartitions=p["nfpartitions"], overlap=p["overlap"], ref=p["ref"],
+    itmax=p["itmax"], relrestol=p["relrestol"],
+    stabilize=p["stabilize"],
+    visualize=p["visualize"])
+
