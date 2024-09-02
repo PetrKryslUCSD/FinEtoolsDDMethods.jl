63172f45f9c9f72ee198cbc4eaaddc396dc40863
Petr Krysl <pkrysl@ucsd.edu>
2024-09-02 13:17:02 -0700

    add a sweep over the partition number

--- examples/conc/shells/zc-weak-grow.sh
+++ examples/conc/shells/zc-weak-grow.sh
@@ -1,16 +1,16 @@
 prefix="zc-weak-grow"
 n1=6
 No=1
-Nep=10000
-Np=4
 Nt=$((n1*(n1-1)*(n1-2)/6))
 for ref in 3 4 5 6  ; do
-    # echo "ref = $ref"
-    N=$(((192*4**(ref-1))))
-    Nep=$((N/Np))
-    # echo "Nep = $Nep"
-    meanNsub=$((Nep/2*6))
-    Nc=$((meanNsub/6/Nt))
-    echo "Np = $Np, Nc = $Nc"
-    julia conc/shells/zc.jl --prefix "$prefix" --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No; 
+    for Np in 4 8 16  ; do
+        # echo "ref = $ref"
+        N=$(((192*4**(ref-1))))
+        Nep=$((N/Np))
+        # echo "Nep = $Nep"
+        meanNsub=$((Nep/2*6))
+        Nc=$((meanNsub/6/Nt))
+        echo "Np = $Np, Nc = $Nc"
+        julia conc/shells/zc.jl --prefix "$prefix" --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No; 
+    done
 done
