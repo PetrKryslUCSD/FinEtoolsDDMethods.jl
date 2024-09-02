379678f90b1a2b4305d58af53dbab3410b445ae7
Petr Krysl <pkrysl@ucsd.edu>
2024-09-02 15:24:44 -0700

    adjust range

--- examples/conc/shells/zc-weak-grow.sh
+++ examples/conc/shells/zc-weak-grow.sh
@@ -4,7 +4,7 @@ Nt=$((n1*(n1+1)*(n1+2)/6)) # three dimensional body
 Nt=$((n1*(n1+1)/2)) # shell
 
 for No in 1 3 5 ; do
-    for ref in  4 5 6 7 ; do
+    for ref in  5 6 7 ; do
         for Np in 4 8 16  ; do
             # echo "ref = $ref"
             N=$(((192*4**(ref-1))))
