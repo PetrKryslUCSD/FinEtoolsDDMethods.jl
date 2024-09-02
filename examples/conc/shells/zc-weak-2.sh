6ee7c60be4d54a148dec8c0679cdfb2deccb8c53
Petr Krysl <pkrysl@ucsd.edu>
2024-09-02 09:04:20 -0700

    add alternative weak z

---
+++ examples/conc/shells/zc-weak-2.sh
@@ -0,0 +1,9 @@
+n1=6
+No=1
+Nep=1000
+for ref in $(seq 4 8) ; do
+    Np=$(((192*4**(ref-1)) / Nep))
+    Nc=$((Np))
+    echo "Np = $Np"
+    julia conc/shells/zc.jl --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No; 
+done
