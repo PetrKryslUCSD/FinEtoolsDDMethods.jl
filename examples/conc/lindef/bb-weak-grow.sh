97945b3f95b7e6019ebc3664ba4e1f73583c38f4
Petr Krysl <pkrysl@ucsd.edu>
2024-09-03 10:23:18 -0700

    clean up

--- examples/conc/lindef/bb-weak-grow.sh
+++ examples/conc/lindef/bb-weak-grow.sh
@@ -1,12 +1,10 @@
 prefix="bb-weak-grow"
 n1=6
-# Nt=$((n1*(n1+1)*(n1+2)/6)) # three dimensional body
-# Nt=$((n1*(n1+1)/2)) # shell
+Nc=0
 
 for No in 1 3 5 ; do
     for N in $(seq 3 10) ; do
         for Np in 4 8 16  ; do
-            Nc=0
             filename="${prefix}-N=$N-Np=$Np-No=$No.json"
             echo $filename
             julia conc/lindef/bb.jl --filename "$filename" \
