d6e81c062f82f71e16b263807893d9160a966eb8
Petr Krysl <pkrysl@ucsd.edu>
2024-09-04 08:24:04 -0700

    remove as

--- examples/conc/shells/zc-strong-grow.sh
+++ examples/conc/shells/zc-strong-grow.sh
@@ -5,7 +5,7 @@ ref=7
 
 for No in 1 3 5; do
     for Np in 4 8 16 32 64 128 ; do
-        filename="${prefix}-ref=$ref-as=$as-Np=$Np-No=$No.json"
+        filename="${prefix}-ref=$ref-Np=$Np-No=$No.json"
         julia conc/shells/zc.jl --filename "$filename" \
             --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No; 
     done
