1576a63e652a75e40e5eb761252ff0b67840c11b
Petr Krysl <pkrysl@ucsd.edu>
2024-09-03 18:05:54 -0700

    mixes up aspects

--- examples/conc/shells/hyp-strong-const.sh
+++ examples/conc/shells/hyp-strong-const.sh
@@ -3,7 +3,7 @@ n1=6
 Nc=500
 ref=7
 
-for as in 10 100 1000 10000; do
+for as in 10000 10 100 1000 ; do
     for No in 1 3 5; do
         for Np in 4 8 16 32 64 128 ; do
             filename="${prefix}-ref=$ref-as=$as-Np=$Np-No=$No.json"
