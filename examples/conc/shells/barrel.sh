599f67abecb99d9c2cd26b0225c6131d712a6463
Petr Krysl <pkrysl@ucsd.edu>
2024-09-04 15:22:34 -0700

    update scripts

--- examples/conc/shells/barrel.sh
+++ examples/conc/shells/barrel.sh
@@ -1,13 +1,19 @@
-script=barrel
-for rf in 1; do
-    for Np in 8 16 32; do
-        for n1 in 6; do
-            for Nc in 400; do
-                for No in 1 3 5; do
-                   julia conc/shells/${script}.jl --nelperpart $ne --nbf1max $n1 --ref $rf --nfpartitions $nf --overlap $ov  --stabilize false; 
+ref=1
+n1=6
+
+for Np in 8 16 32; do
+        for Nc in 0 400; do
+            for No in 1 3 5; do
+                for st in false true; do
+                    filename="barrel-st=$st-Nc=$Nc-Np=$Np-No=$No.json"
+                    julia conc/shells/barrel.jl  \
+                        --filename "$filename" \
+                        --stabilize $st \
+                        --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No; 
                 done
-            done 
-        done
+            done
+        done 
     done
 done
+
 
