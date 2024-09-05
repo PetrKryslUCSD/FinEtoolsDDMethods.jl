c5bd5f09b0c1a20814f13ed2e1d66e21612a07fb
Petr Krysl <pkrysl@ucsd.edu>
2024-09-05 15:25:34 -0700

    a

--- examples/conc/lindef/bb-weak-grow.sh
+++ examples/conc/lindef/bb-weak-grow.sh
@@ -9,7 +9,7 @@ for N in $(seq 2 9) ; do
     Ne=$((7*12*21*N**3)) # (6840/2)
     Ne2=$((7*12*21*2**3))
     Np=$((Ne/Ne2*2)) # (6840/2)
-    Nc=$((Ne/Np*3/nt/3)) # (6840/2)
+    Nc=$((Ne/Np*3/nt/3/2)) # (6840/2)
     filename="${prefix}-N=$N.json"
     echo "N=$N Nc=$Nc Np=$Np $filename "
     julia conc/lindef/bb.jl --filename "$filename" \
