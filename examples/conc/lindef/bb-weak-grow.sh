8996807007970ee41f7e057ba5402741b8f23625
Petr Krysl <pkrysl@ucsd.edu>
2024-09-05 12:55:16 -0700

    reformat weak

--- examples/conc/lindef/bb-weak-grow.sh
+++ examples/conc/lindef/bb-weak-grow.sh
@@ -1,14 +1,20 @@
 prefix="bb-weak-grow"
 n1=6
 Nc=0
-
-for No in 1 3 5 ; do
-    for N in $(seq 3 10) ; do
-        for Np in 4 8 16  ; do
-            filename="${prefix}-N=$N-Np=$Np-No=$No.json"
-            echo $filename
-            julia conc/lindef/bb.jl --filename "$filename" \
-                --N $N --Nc $Nc --n1 $n1 --Np $Np --No $No; 
-        done
-    done
+No=5
+# for No in 5 ; do
+#     for N in 2 4 8 ; do
+#         for Np in 4 8 16  ; do
+#             filename="${prefix}-N=$N-Np=$Np-No=$No.json"
+#             echo $filename
+#             julia conc/lindef/bb.jl --filename "$filename" \
+#                 --N $N --Nc $Nc --n1 $n1 --Np $Np --No $No; 
+#         done
+#     done
+# done
+for N in 2 4 8 16 ; do
+    Np=$N
+    filename="${prefix}-N=$N.json"
+    julia conc/lindef/bb.jl --filename "$filename" \
+                    --N $N --Nc $Nc --n1 $n1 --Np $Np --No $No; 
 done
