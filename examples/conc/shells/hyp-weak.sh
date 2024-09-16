1ef0e2f722925a9d4635b04f39443f12ac3fabf2
Petr Krysl <pkrysl@ucsd.edu>
2024-09-16 09:05:52 -0700

    add hyp weak

---
+++ examples/conc/shells/hyp-weak.sh
@@ -0,0 +1,13 @@
+prefix="hyp-weak"
+n1=6
+Nc=0
+No=5
+as=10
+
+for ref in 4 8 16 ; do
+    Np=$((16*ref))
+    filename="${prefix}-ref=$ref-as=$as-Np=$Np-No=$No.json"
+    julia conc/shells/hyp.jl --filename "$filename" \
+    --aspect $as \
+    --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No; 
+done
