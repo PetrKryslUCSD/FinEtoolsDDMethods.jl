4f407b7da5d547e4638311abeae3519df205e872
Petr Krysl <pkrysl@ucsd.edu>
2024-09-16 15:42:48 -0700

    print detailed element list length

--- examples/conc/shells/hyp_free_seq_examples.jl
+++ examples/conc/shells/hyp_free_seq_examples.jl
@@ -132,7 +132,9 @@ function _execute(filename, ncoarse, ref, aspect, Nc, n1, Np, No, itmax, relrest
     minps = minimum(partition_sizes)
     maxps = maximum(partition_sizes)
     @info "Min, Mean, Max fine partition size: $(minps), $(meanps) $(maxps)"
-    @info "Element list lengths: $([length(_el.nonoverlapping) for _el in cpi.element_lists])"
+    @info "Element list lengths (noneoverlapping): $([length(_el.nonoverlapping) for _el in cpi.element_lists])"
+    @info "Element list lengths (overlapping): $([length(_el.overlapping) for _el in cpi.element_lists])"
+    @info "Element list lengths (all connected): $([length(_el.all_connected) for _el in cpi.element_lists])"
     @info "Create partitions ($(round(time() - t1, digits=3)) [s])"
 
     t1 = time()
