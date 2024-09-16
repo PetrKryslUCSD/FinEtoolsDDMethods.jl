50a16f1e675aec87878299def75e48ee5a4967a3
Petr Krysl <pkrysl@ucsd.edu>
2024-09-16 15:22:28 -0700

    use minimum maximum

--- examples/conc/shells/hyp_free_seq_examples.jl
+++ examples/conc/shells/hyp_free_seq_examples.jl
@@ -129,8 +129,8 @@ function _execute(filename, ncoarse, ref, aspect, Nc, n1, Np, No, itmax, relrest
     partition_list  = make_partitions(cpi, fes, make_matrix, nothing)
     partition_sizes = [partition_size(_p) for _p in partition_list]
     meanps = mean(partition_sizes)
-    minps = min(partition_sizes)
-    maxps = max(partition_sizes)
+    minps = minimum(partition_sizes)
+    maxps = maximum(partition_sizes)
     @info "Min, Mean, Max fine partition size: $(minps), $(meanps) $(maxps)"
     @info "Create partitions ($(round(time() - t1, digits=3)) [s])"
 
