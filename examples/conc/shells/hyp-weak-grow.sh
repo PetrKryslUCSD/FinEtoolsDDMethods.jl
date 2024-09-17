e7d1e27932e46baf4b8a05de7cc286b8011e1652
Petr Krysl <pkrysl@ucsd.edu>
2024-09-16 21:06:52 -0700

    add grow

---
+++ examples/conc/shells/hyp-weak-grow.sh
@@ -0,0 +1,15 @@
+prefix="hyp-weak-grow"
+n1=6
+No=5
+as=10
+
+for as in 10 100 ; do
+    for ref in $(seq 4 20) ; do
+        Np=$((ref**2/2))
+        Nc=$((2*Np))
+        filename="${prefix}-ref=$ref-as=$as-Np=$Np-No=$No.json"
+        julia conc/shells/hyp.jl --filename "$filename" \
+        --aspect $as \
+        --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No; 
+    done
+done
