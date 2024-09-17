255f313e5516d3747e64ff0fb14fd8b03d118f0f
Petr Krysl <pkrysl@ucsd.edu>
2024-09-17 15:16:44 -0700

    fix script

--- examples/conc/shells/zc-weak-growlog.jl
+++ examples/conc/shells/zc-weak-growlog.jl
@@ -6,7 +6,6 @@ No=5
 for ref in 4:8
     Np = Int(round(ref^2 / 2))
     filename = "$(prefix)-ref=$ref-Np=$Np-No=$No.json"
-    echo$filename
     run(`
     julia --project=. conc/shells/zc.jl --filename "$filename" --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No
     `)
