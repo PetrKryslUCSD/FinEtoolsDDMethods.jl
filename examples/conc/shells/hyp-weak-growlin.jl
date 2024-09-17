b381509564aa5d2c4f8c8bbbce0f6a7a60e718ee
Petr Krysl <pkrysl@ucsd.edu>
2024-09-17 08:06:51 -0700

    increase size

--- examples/conc/shells/hyp-weak-growlin.jl
+++ examples/conc/shells/hyp-weak-growlin.jl
@@ -5,7 +5,7 @@ No=5
 for as in [10 100]
     for ref in 4:20
         Np = Int(round(ref^2 / 2))
-        Nc = Int(ceil(Np / 2))
+        Nc = Int(ceil(Np * 2))
         filename="$(prefix)-ref=$ref-as=$as-Np=$Np-No=$No.json"
         run(`
         julia --project=. conc/shells/hyp.jl --filename "$filename" --aspect $as --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No
