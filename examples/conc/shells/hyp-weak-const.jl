b5760de9400e45617fd443ca520cbdb3281bc6ce
Petr Krysl <pkrysl@ucsd.edu>
2024-09-17 08:22:05 -0700

    add cost

---
+++ examples/conc/shells/hyp-weak-const.jl
@@ -0,0 +1,14 @@
+prefix="hyp-weak-const"
+n1=6
+No=5
+
+for as in [10 100]
+    for ref in 4:20
+        Np = Int(round(ref^2 / 2))
+        Nc = 0
+        filename="$(prefix)-ref=$ref-as=$as-Np=$Np-No=$No.json"
+        run(`
+        julia --project=. conc/shells/hyp.jl --filename "$filename" --aspect $as --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No
+        `)
+    end
+end
