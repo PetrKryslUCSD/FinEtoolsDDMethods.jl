c77af6aed53a923bfcaa3c9d28ac916413ff3e35
Petr Krysl <pkrysl@ucsd.edu>
2024-09-17 15:18:33 -0700

    fix rule

--- examples/conc/shells/zc-weak-growlog.jl
+++ examples/conc/shells/zc-weak-growlog.jl
@@ -1,10 +1,10 @@
 prefix="zc-weak-growlog"
 n1=6
-Nc=0
 No=5
 
 for ref in 4:8
     Np = Int(round(ref^2 / 2))
+    Nc = Int(ceil(100 * log(Np / 4)))
     filename = "$(prefix)-ref=$ref-Np=$Np-No=$No.json"
     run(`
     julia --project=. conc/shells/zc.jl --filename "$filename" --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No
