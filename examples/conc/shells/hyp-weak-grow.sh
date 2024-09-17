5cf998ab4a2af48172de793428f4410096004a3e
Petr Krysl <pkrysl@ucsd.edu>
2024-09-17 07:29:30 -0700

    clean up script

--- examples/conc/shells/hyp-weak-grow.sh
+++ examples/conc/shells/hyp-weak-grow.sh
@@ -1,7 +1,6 @@
 prefix="hyp-weak-grow"
 n1=6
 No=5
-as=10
 
 for as in 10 100 ; do
     for ref in $(seq 4 20) ; do
@@ -10,6 +9,6 @@ for as in 10 100 ; do
         filename="${prefix}-ref=$ref-as=$as-Np=$Np-No=$No.json"
         julia conc/shells/hyp.jl --filename "$filename" \
         --aspect $as \
-        --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No; 
+        --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No;
     done
 done
--- examples/conc/shells/hyp-weak-growlog.jl
+++ examples/conc/shells/hyp-weak-growlog.jl
@@ -1,14 +1,13 @@
-prefix="hyp-weak-grow"
+prefix="hyp-weak-growlog"
 n1=6
 No=5
 
 for as in [10 100]
-    for ref in 4:5
+    for ref in 4:20
         Np = Int(round(ref^2 / 2))
         Nc = Int(ceil(log(10*Np)))
         @info "$Np $(log(10*Np)) $Nc"
         filename="$(prefix)-ref=$ref-as=$as-Np=$Np-No=$No.json"
-        # "bash julia --project=. hyp.jl --filename "$filename" --aspect $as --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No"
         run(`
         julia --project=. conc/shells/hyp.jl --filename "$filename" --aspect $as --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No
         `)
