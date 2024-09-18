3b582a01099bbdb22291b3989d1eb991ecba7da3
Petr Krysl <pkrysl@ucsd.edu>
2024-09-18 16:23:07 -0700

    loop over the size of the partition

--- examples/conc/shells/zc-weak-growlin.jl
+++ examples/conc/shells/zc-weak-growlin.jl
@@ -5,15 +5,16 @@ nt = n1 * (n1 + 1) / 2 * 6
 Nepp = 5000
 number_of_clusters(Np) = Int(ceil(Np / 2))
 
-
-for ref in 20:10:140
-    @show Ne = Int(round(48 * ref^2))
-    @show Np = Int(round(Ne / Nepp))
-    @show Nc = number_of_clusters(Np)
-    filename = "$(prefix)-ref=$ref-Np=$Np-No=$No.json"
-    n = sqrt(Ne / Np) / 2
-@show     nd = (2 * n^2 + 4 * (5 - 1) * 2 * n) * 6
-    run(`
-    julia --project=. conc/shells/zc.jl --filename "$filename" --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No
-    `)
+for Nepp in [5000, 1000, 200]
+    for ref in 20:10:140
+        @show Ne = Int(round(48 * ref^2))
+        @show Np = Int(round(Ne / Nepp))
+        @show Nc = number_of_clusters(Np)
+        filename = "$(prefix)-ref=$ref-Np=$Np-No=$No-Nepp=$Nepp.json"
+        n = sqrt(Ne / Np) / 2
+        @show nd = (2 * n^2 + 4 * (5 - 1) * 2 * n) * 6
+        run(`
+        julia --project=. conc/shells/zc.jl --filename "$filename" --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No
+        `)
+    end
 end
--- examples/conc/shells/zc-weak-growlog.jl
+++ examples/conc/shells/zc-weak-growlog.jl
@@ -5,12 +5,14 @@ nt = n1 * (n1 + 1) / 2 * 6
 Nepp = 5000
 number_of_clusters(Np) = Int(ceil(10 * log(Np / 2)))
 
-for ref in 20:10:140
-    Ne = Int(round(48 * ref^2))
-    Np = Int(round(Ne / Nepp))
-    Nc = number_of_clusters(Np)
-    filename = "$(prefix)-ref=$ref-Np=$Np-No=$No.json"
-    run(`
-    julia --project=. conc/shells/zc.jl --filename "$filename" --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No
-    `)
+for Nepp in [5000, 1000, 200]
+    for ref in 20:10:140
+        Ne = Int(round(48 * ref^2))
+        Np = Int(round(Ne / Nepp))
+        Nc = number_of_clusters(Np)
+        filename = "$(prefix)-ref=$ref-Np=$Np-No=$No-Nepp=$Nepp.json"
+        run(`
+        julia --project=. conc/shells/zc.jl --filename "$filename" --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No
+        `)
+    end
 end
