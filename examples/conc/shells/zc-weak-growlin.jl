c8cad4a13362c71fbe921a4ee0054f168139d719
Petr Krysl <pkrysl@ucsd.edu>
2024-09-18 07:48:43 -0700

    adjust calculation of the number of clusters

--- examples/conc/shells/zc-weak-growlin.jl
+++ examples/conc/shells/zc-weak-growlin.jl
@@ -1,10 +1,13 @@
 prefix="zc-weak-growlin"
-n1=6
-No=5
+n1 = 6
+No = 5
+Ndof8 = 9500000
+nt = n1 * (n1 + 1) / 2 * 6
+number_of_clusters(Np) = Int(ceil(Np / (Ndof8 /  (4^8/32) / nt) * 10))
 
 for ref in 4:8
     Np = Int(round(4 ^ ref / 32))
-    Nc = Int(ceil(Np * 2))
+    Nc = number_of_clusters(Np)
     filename = "$(prefix)-ref=$ref-Np=$Np-No=$No.json"
     run(`
     julia --project=. conc/shells/zc.jl --filename "$filename" --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No
