f5eb71a700a3e4b13ba25758c5cbfdf930bfcdd0
Petr Krysl <pkrysl@ucsd.edu>
2024-09-18 15:43:01 -0700

    update grow log

--- examples/conc/shells/zc-weak-growlog.jl
+++ examples/conc/shells/zc-weak-growlog.jl
@@ -2,13 +2,15 @@ prefix="zc-weak-growlog"
 n1 = 6
 No = 5
 nt = n1 * (n1 + 1) / 2 * 6
-number_of_clusters(Np) = Int(ceil(10 * log(Np / 4)))
+Nepp = 5000
+number_of_clusters(Np) = Int(ceil(10 * log(Np / 2)))
 
-for ref in 4:8
-    Np = Int(round(4 ^ ref / 32))
+for ref in 20:10:140
+    Ne = Int(round(48 * ref^2))
+    Np = Int(round(Ne / Nepp))
     Nc = number_of_clusters(Np)
     filename = "$(prefix)-ref=$ref-Np=$Np-No=$No.json"
-    run(`
-    julia --project=. conc/shells/zc.jl --filename "$filename" --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No
-    `)
+    # run(`
+    # julia --project=. conc/shells/zc.jl --filename "$filename" --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No
+    # `)
 end
