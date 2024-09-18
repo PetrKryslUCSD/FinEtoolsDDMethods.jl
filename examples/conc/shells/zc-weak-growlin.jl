ffd71c6906f508158c9cf46ad22af92302a3a69b
Petr Krysl <pkrysl@ucsd.edu>
2024-09-18 14:43:10 -0700

    adjust

--- examples/conc/shells/zc-weak-growlin.jl
+++ examples/conc/shells/zc-weak-growlin.jl
@@ -2,14 +2,18 @@ prefix="zc-weak-growlin"
 n1 = 6
 No = 5
 nt = n1 * (n1 + 1) / 2 * 6
+# n = sqrt(Ne / Np)
+#     nd = (2 * n^2 + 4 * 5 * 2 * n) * 6
 Nepp = 5000
-number_of_clusters(Np) = Int(ceil(Nepp * 6 / nt / 2))
+number_of_clusters(Np) = Int(ceil(Np / 2))
 
-for ref in 10:20
-    Np = Int(round(48 * ref^2 / Nepp))
-    Nc = number_of_clusters(Np)
+
+for ref in 20:10:100
+    @show Ne = Int(round(48 * ref^2))
+    @show Np = Int(round(Ne / Nepp))
+    @show Nc = number_of_clusters(Np)
     filename = "$(prefix)-ref=$ref-Np=$Np-No=$No.json"
-    run(`
-    julia --project=. conc/shells/zc.jl --filename "$filename" --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No
-    `)
+    # run(`
+    # julia --project=. conc/shells/zc.jl --filename "$filename" --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No
+    # `)
 end
