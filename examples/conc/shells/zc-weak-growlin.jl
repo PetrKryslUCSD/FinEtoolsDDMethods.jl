7dc5e7fb9a7fde63ee3fb4220d810f956398ca53
Petr Krysl <pkrysl@ucsd.edu>
2024-09-18 14:23:37 -0700

    a just range

--- examples/conc/shells/zc-weak-growlin.jl
+++ examples/conc/shells/zc-weak-growlin.jl
@@ -5,7 +5,7 @@ Ndof8 = 9500000
 nt = n1 * (n1 + 1) / 2 * 6
 number_of_clusters(Np) = Int(ceil(Np / (Ndof8 /  (4^8/32) / nt) * 10))
 
-for ref in 8:20
+for ref in 10:20
     Np = Int(round(48 * ref^2 / 5000))
     Nc = number_of_clusters(Np)
     filename = "$(prefix)-ref=$ref-Np=$Np-No=$No.json"
