be3b36230918c9bf76189a8262446510c4c91150
Petr Krysl <pkrysl@ucsd.edu>
2024-09-17 19:02:59 -0700

    add

---
+++ examples/conc/shells/zc-weak-match.jl
@@ -0,0 +1,12 @@
+prefix="zc-weak-match"
+n1=6
+No=5
+
+for ref in 4:8
+    Np = Int(round(4 ^ ref / 32))
+    Nc = 0
+    filename = "$(prefix)-ref=$ref-Np=$Np-No=$No.json"
+    run(`
+    julia --project=. conc/shells/zc.jl --filename "$filename" --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No
+    `)
+end
