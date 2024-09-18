7af57173160c9ab6f71850b457bbb8b681a3a142
Petr Krysl <pkrysl@ucsd.edu>
2024-09-18 14:25:41 -0700

    fix function

--- examples/conc/shells/zc-weak-growlin.jl
+++ examples/conc/shells/zc-weak-growlin.jl
@@ -1,9 +1,8 @@
 prefix="zc-weak-growlin"
 n1 = 6
 No = 5
-Ndof8 = 9500000
 nt = n1 * (n1 + 1) / 2 * 6
-number_of_clusters(Np) = Int(ceil(Np / (Ndof8 /  (4^8/32) / nt) * 10))
+number_of_clusters(Np) = Int(ceil(Np / nt) * 2)
 
 for ref in 10:20
     Np = Int(round(48 * ref^2 / 5000))
