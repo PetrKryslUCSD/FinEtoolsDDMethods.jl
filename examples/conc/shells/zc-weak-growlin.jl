8305c86f63e703fd96501a076c0291a42d4c45b7
Petr Krysl <pkrysl@ucsd.edu>
2024-09-18 14:28:09 -0700

    use number of elements per partition

--- examples/conc/shells/zc-weak-growlin.jl
+++ examples/conc/shells/zc-weak-growlin.jl
@@ -2,10 +2,11 @@ prefix="zc-weak-growlin"
 n1 = 6
 No = 5
 nt = n1 * (n1 + 1) / 2 * 6
-number_of_clusters(Np) = Int(ceil(Np / nt) * 2)
+Nepp = 5000
+number_of_clusters(Np) = Int(ceil(Nepp * 6 / nt) * 2)
 
 for ref in 10:20
-    Np = Int(round(48 * ref^2 / 5000))
+    Np = Int(round(48 * ref^2 / Nepp))
     Nc = number_of_clusters(Np)
     filename = "$(prefix)-ref=$ref-Np=$Np-No=$No.json"
     run(`
