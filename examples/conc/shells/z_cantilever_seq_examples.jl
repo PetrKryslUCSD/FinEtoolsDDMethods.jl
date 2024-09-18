cd0c63127aa710f21e2a45c8dcb6a28085dd1064
Petr Krysl <pkrysl@ucsd.edu>
2024-09-18 14:19:27 -0700

    implement alternate refinement

--- examples/conc/shells/z_cantilever_seq_examples.jl
+++ examples/conc/shells/z_cantilever_seq_examples.jl
@@ -240,8 +240,201 @@ function _execute(filename, ref, Nc, n1, Np, No, itmax, relrestol, peek, visuali
     true
 end
 
-function test(;filename = "", ref = 2, Nc = 2, n1 = 6, Np = 2, No = 1, itmax = 2000, relrestol = 1e-6, peek = false, visualize = false) 
-    _execute(filename, ref, Nc, n1, Np, No, itmax, relrestol, peek, visualize)
+function _execute_alt(filename, ref, Nc, n1, Np, No, itmax, relrestol, peek, visualize)
+    CTE = 0.0
+    thickness = 0.1
+    
+    tolerance = thickness/1000
+    xyz = Float64[
+    0 -1 -1;
+    2.5 -1 -1;
+    10 -1 -1;
+    0 -1 0;
+    2.5 -1 0;
+    10 -1 0;
+    0 1 0;
+    2.5 1 0;
+    10 1 0;
+    0 1 1;
+    2.5 1 1;
+    10 1 1;
+    ]
+    fens1, fes1 = Q4quadrilateral(xyz[[1, 2, 5, 4], :], ref * 2, ref * 1) 
+    fens2, fes2 = Q4quadrilateral(xyz[[4, 5, 8, 7], :], ref * 2, ref * 1) 
+    fens, fes1, fes2 = mergemeshes(fens1, fes1,  fens2, fes2, tolerance)
+    fes = cat(fes1, fes2)
+
+    fens1, fes1 = fens, fes 
+    fens2, fes2 = Q4quadrilateral(xyz[[7, 8, 11, 10], :], ref * 2, ref * 1) 
+    fens, fes1, fes2 = mergemeshes(fens1, fes1,  fens2, fes2, tolerance)
+    fes = cat(fes1, fes2)
+
+    fens1, fes1 = fens, fes 
+    fens2, fes2 = Q4quadrilateral(xyz[[2, 3, 6, 5], :], ref * 6, ref * 1) 
+    fens, fes1, fes2 = mergemeshes(fens1, fes1,  fens2, fes2, tolerance)
+    fes = cat(fes1, fes2)
+
+    fens1, fes1 = fens, fes 
+    fens2, fes2 = Q4quadrilateral(xyz[[5, 6, 9, 8], :], ref * 6, ref * 1) 
+    fens, fes1, fes2 = mergemeshes(fens1, fes1,  fens2, fes2, tolerance)
+    fes = cat(fes1, fes2)
+
+    fens1, fes1 = fens, fes 
+    fens2, fes2 = Q4quadrilateral(xyz[[8, 9, 12, 11], :], ref * 6, ref * 1) 
+    fens, fes1, fes2 = mergemeshes(fens1, fes1,  fens2, fes2, tolerance)
+    fes = cat(fes1, fes2)
+
+    fens, fes = Q4toT3(fens, fes)
+    
+    MR = DeforModelRed3D
+    mater = MatDeforElastIso(MR, 0.0, E, nu, CTE)
+    ocsys = CSys(3, 3, zcant!)
+
+    sfes = FESetShellT3()
+    accepttodelegate(fes, sfes)
+    femm = FEMMShellT3FFModule.make(IntegDomain(fes, TriRule(1), thickness), mater)
+    stiffness = FEMMShellT3FFModule.stiffness
+    associategeometry! = FEMMShellT3FFModule.associategeometry!
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    # plane of symmetry perpendicular to Z
+    l1 = selectnode(fens; box = Float64[0 0 -Inf Inf -Inf Inf], inflate = tolerance)
+    for i in [1,2,3,]
+        setebc!(dchi, l1, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    fr = dofrange(dchi, DOF_KIND_FREE)
+    dr = dofrange(dchi, DOF_KIND_DATA)
+    
+    @info("Refinement factor: $(ref)")
+    @info("Number of fine grid partitions: $(Np)")
+    @info("Number of overlaps: $(No)")
+    @info("Number of elements: $(count(fes))")
+    @info("Number of free dofs = $(nfreedofs(dchi))")
+
+    bfes = meshboundary(fes)
+    el = selectelem(fens, bfes, box = Float64[10.0 10.0 1.0 1.0 -1 1], tolerance = tolerance)
+    lfemm1 = FEMMBase(IntegDomain(subset(bfes, el), GaussRule(1, 2)))
+    fi1 = ForceIntensity(Float64[0, 0, +0.6e6, 0, 0, 0]);
+    el = selectelem(fens, bfes, box = Float64[10.0 10.0 -1.0 -1.0 -1 1], tolerance = tolerance)
+    lfemm2 = FEMMBase(IntegDomain(subset(bfes, el), GaussRule(1, 2)))
+    fi2 = ForceIntensity(Float64[0, 0, -0.6e6, 0, 0, 0]);
+    F = distribloads(lfemm1, geom0, dchi, fi1, 1) + distribloads(lfemm2, geom0, dchi, fi2, 1);
+    F_f = F[fr]
+
+    associategeometry!(femm, geom0)
+    
+    function make_matrix(fes)
+        femm1 = deepcopy(femm) # for thread safety
+        femm1.integdomain.fes = fes
+        return stiffness(femm1, geom0, u0, Rfield0, dchi);
+    end
+
+    t1 = time()
+    cpi = CoNCPartitioningInfo(fens, fes, Np, No, dchi) 
+    partition_list  = make_partitions(cpi, fes, make_matrix, nothing)
+    partition_sizes = [partition_size(_p) for _p in partition_list]
+    meanps = mean(partition_sizes)
+    @info "Mean fine partition size: $(meanps)"
+    @info "Create partitions ($(round(time() - t1, digits=3)) [s])"
+
+    t1 = time()
+    @info("Number of clusters (requested): $(Nc)")
+    @info("Number of 1D basis functions: $(n1)")
+    nt = n1*(n1+1)/2 
+    (Nc == 0) && (Nc = Int(floor(meanps / nt / ndofs(dchi))))
+    Nepc = count(fes) ÷ Nc
+    (n1 > (Nepc/2)^(1/2)) && @error "Not enough elements per cluster"
+    @info("Number of elements per cluster: $(Nepc)")
+    
+    cpartitioning, Nc = shell_cluster_partitioning(fens, fes, Nepc)
+    @info("Number of clusters (actual): $(Nc)")
+        
+    mor = CoNCData(list -> patch_coordinates(fens.xyz, list), cpartitioning)
+    Phi = transfmatrix(mor, LegendreBasis, n1, dchi)
+    Phi = Phi[fr, :]
+    @info("Size of the reduced problem: $(size(Phi, 2))")
+    @info "Generate clusters ($(round(time() - t1, digits=3)) [s])"
+
+    function peeksolution(iter, x, resnorm)
+        peek && (@info("it $(iter): residual norm =  $(resnorm)"))
+    end
+    
+    t1 = time()
+    Kr_ff = spzeros(size(Phi, 2), size(Phi, 2))
+    for partition in partition_list
+        Kr_ff += (Phi' * partition.nonoverlapping_K * Phi)
+    end
+    Krfactor = lu(Kr_ff)
+    @info "Create global factor ($(round(time() - t1, digits=3)) [s])"
+    
+    t0 = time()
+    M! = preconditioner!(Krfactor, Phi, partition_list)
+    (u_f, stats) = pcg_seq(
+        (q, p) -> partition_multiply!(q, partition_list, p), 
+        F_f, zeros(size(F_f));
+        (M!)=(q, p) -> M!(q, p),
+        peeksolution=peeksolution,
+        itmax=itmax, 
+        # atol=0, rtol=relrestol, normtype = KSP_NORM_NATURAL
+        # atol=relrestol * norm(F_f), rtol=0, normtype = KSP_NORM_NATURAL
+        atol= 0, rtol=relrestol, normtype = KSP_NORM_UNPRECONDITIONED
+        )
+    t1 = time()
+    @info("Number of iterations:  $(stats.niter)")
+    @info "Iterations ($(round(t1 - t0, digits=3)) [s])"
+    stats = (niter = stats.niter, residuals = stats.residuals ./ norm(F_f))
+    data = Dict(
+        "number_nodes" => count(fens),
+        "number_elements" => count(fes),
+        "nfreedofs" => nfreedofs(dchi),
+        "Nc" => Nc,
+        "Np" => Np,
+        "No" => No,
+        "meanps" => meanps,
+        "size_Kr_ff" => size(Krfactor),
+        "stats" => stats,
+        "iteration_time" => t1 - t0,
+    )
+    f = (filename == "" ?
+         "z_cantilever-" *
+         "-ref=$(ref)" *
+         "-Nc=$(Nc)" *
+         "-n1=$(n1)" *
+         "-Np=$(Np)" *
+         "-No=$(No)" :
+         filename)
+    @info "Storing data in $(f * ".json")"
+    DataDrop.store_json(f * ".json", data)
+    scattersysvec!(dchi, u_f)
+    
+    if visualize
+        f = (filename == "" ?
+         "z_cantilever-" *
+         "-ref=$(ref)" *
+         "-Nc=$(Nc)" *
+         "-n1=$(n1)" *
+         "-Np=$(Np)" *
+         "-No=$(No)" :
+         filename) * "-cg-sol"
+        VTK.vtkexportmesh(f * ".vtk", fens, fes;
+            vectors=[("u", deepcopy(dchi.values[:, 1:3]),)])
+    end
+
+    true
+end
+
+function test(;filename = "", ref = 8, Nc = 2, n1 = 6, Np = 2, No = 1, itmax = 2000, relrestol = 1e-6, peek = false, visualize = false) 
+    _execute_alt(filename, ref, Nc, n1, Np, No, itmax, relrestol, peek, visualize)
 end
 
 nothing
