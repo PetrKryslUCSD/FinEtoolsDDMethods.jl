09d7ea95ca69920f05013b457cfd950dbeb17607
Petr Krysl <pkrysl@ucsd.edu>
2024-09-19 21:27:26 -0700

    fix not defined

--- examples/conc/lindef/fib-strong-hex-cni-grow.jl
+++ examples/conc/lindef/fib-strong-hex-cni-grow.jl
@@ -11,8 +11,8 @@ num = 0.4999
 number_of_clusters(Np) = Int(ceil(Np / 2))
 
 for Np in [64, 128, 256, 512, ]
-    filename = "$(prefix)-ref=$ref-Np=$Np-No=$No-Nc=$Nc.json"
     Nc = number_of_clusters(Np)
+    filename = "$(prefix)-ref=$ref-Np=$Np-No=$No-Nc=$Nc.json"
     run(`
     julia --project=. conc/lindef/fib.jl --filename "$filename" --kind $kind --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No --Ef $Ef --nuf $nuf --Em $Em --num $num
     `)
