6dad5d9226f65dcb1155528a501d3d3ec61a6f73
Petr Krysl <pkrysl@ucsd.edu>
2024-09-19 20:32:54 -0700

    correct the range

--- examples/conc/lindef/fib-strong-hex-cni-match.jl
+++ examples/conc/lindef/fib-strong-hex-cni-match.jl
@@ -10,7 +10,7 @@ nuf = 0.3
 Em = 1000.0
 num = 0.4999
 
-for Np in 8:8:64
+for Np in [64, 128, 256, 512, ]
     filename = "$(prefix)-ref=$ref-Np=$Np-No=$No-Nc=$Nc.json"
     run(`
     julia --project=. conc/lindef/fib.jl --filename "$filename" --kind $kind --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No --Ef $Ef --nuf $nuf --Em $Em --num $num
