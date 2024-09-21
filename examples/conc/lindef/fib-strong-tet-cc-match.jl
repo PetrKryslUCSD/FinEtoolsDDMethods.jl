6be0fcf6d5d24a3b3140674e26413bd32c7e3381
Petr Krysl <pkrysl@ucsd.edu>
2024-09-21 09:14:05 -0700

    add cc

---
+++ examples/conc/lindef/fib-strong-tet-cc-match.jl
@@ -0,0 +1,18 @@
+kind = "tet"
+prefix = "fib-strong-$(kind)-cc-match"
+n1 = 6
+No = 2
+nt = n1 * (n1 + 1) * (n1 + 2) / 6 * 3
+ref = 4
+Nc = 0
+Ef = 100000.0
+nuf = 0.3
+Em = 1000.0
+num = 0.3
+
+for Np in [64, 128, 256, 512, ]
+    filename = "$(prefix)-ref=$ref-Np=$Np-No=$No-Nc=$Nc.json"
+    run(`
+    julia --project=. conc/lindef/fib.jl --filename "$filename" --kind $kind --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No --Ef $Ef --nuf $nuf --Em $Em --num $num
+    `)
+end
