c601138cacef888a21ab645f5e28778a2fc9c5e2
Petr Krysl <pkrysl@ucsd.edu>
2024-09-25 14:52:53 -0700

    target timing

--- src/DDCoNCSeqModule.jl
+++ src/DDCoNCSeqModule.jl
@@ -27,9 +27,9 @@ using ShellStructureTopo
 using ..PartitionCoNCModule: CoNCPartitioningInfo, CoNCPartitionData, npartitions
 
 function make_partitions(cpi, fes, make_matrix, make_interior_load)
-    partition_list  = [CoNCPartitionData(cpi) for i in 1:npartitions(cpi)]
+    @time partition_list  = [CoNCPartitionData(cpi) for i in 1:npartitions(cpi)]
     for i in eachindex(partition_list)
-        partition_list[i] = CoNCPartitionData(cpi, i, fes, make_matrix, make_interior_load)
+        @time partition_list[i] = CoNCPartitionData(cpi, i, fes, make_matrix, make_interior_load)
     end  
     return partition_list
 end
