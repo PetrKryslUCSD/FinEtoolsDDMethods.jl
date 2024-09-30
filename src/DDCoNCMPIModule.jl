1edda490b153a82ec3649157ec3a4e560deb26b7
Petr Krysl <pkrysl@ucsd.edu>
2024-09-30 07:16:30 -0700

    use non blocking communication

--- src/DDCoNCMPIModule.jl
+++ src/DDCoNCMPIModule.jl
@@ -55,24 +55,31 @@ function precondition_local_solve!(q, partition, p)
     q
 end
 
-function scatter_nvector(v, cpi, comm, rank, partition)
+function scatter_nvector(p, cpi, comm, rank, partition)
     if rank == 0
+        requests = MPI.Request[]
         for i in 1:length(cpi.dof_lists)
             d = cpi.dof_lists[i].nonoverlapping
-            cpi.nbuffs[i] .= v[d]
-            MPI.Send(cpi.nbuffs[i], comm; dest = i)
+            cpi.nbuffs[i] .= p[d]
+            req = MPI.Isend(cpi.nbuffs[i], comm; dest = i)
+            push!(requests, req)
         end
+        MPI.Waitall!(requests)
     else
         MPI.Recv!(partition.ntempp, comm; source = 0)
     end
 end
 
-function gather_nvector(v, cpi, comm, rank, partition)
+function gather_nvector(q, cpi, comm, rank, partition)
     if rank == 0
-        for i in 1:length(cpi.dof_lists)
-            MPI.Recv!(cpi.nbuffs[i], comm; source = i)
+        requests = [MPI.Irecv!(cpi.nbuffs[i], comm; source = i) for i in 1:length(cpi.dof_lists)]
+        while true
+            i = MPI.Waitany(requests)
+            if i === nothing
+                break
+            end
             d = cpi.dof_lists[i].nonoverlapping
-            v[d] .+= cpi.nbuffs[i]
+            q[d] .+= cpi.nbuffs[i]
         end
     else
         MPI.Send(partition.ntempq, comm; dest = 0)
@@ -94,24 +101,31 @@ function partition_mult!(q, cpi, comm, rank, partition, p)
     q
 end
 
-function scatter_ovector(v, cpi, comm, rank, partition)
+function scatter_ovector(p, cpi, comm, rank, partition)
     if rank == 0
+        requests = MPI.Request[]
         for i in 1:length(cpi.dof_lists)
             d = cpi.dof_lists[i].overlapping
-            cpi.obuffs[i] .= v[d]
-            MPI.Send(cpi.obuffs[i], comm; dest = i)
+            cpi.obuffs[i] .= p[d]
+            req = MPI.Isend(cpi.obuffs[i], comm; dest = i)
+            push!(requests, req)
         end
+        MPI.Waitall!(requests)
     else
         MPI.Recv!(partition.otempp, comm; source = 0)
     end
 end
 
-function gather_ovector(v, cpi, comm, rank, partition)
+function gather_ovector(q, cpi, comm, rank, partition)
     if rank == 0
-        for i in 1:length(cpi.dof_lists)
-            MPI.Recv!(cpi.obuffs[i], comm; source = i)
+        requests = [MPI.Irecv!(cpi.obuffs[i], comm; source = i) for i in 1:length(cpi.dof_lists)]
+        while true
+            i = MPI.Waitany(requests)
+            if i === nothing
+                break
+            end
             d = cpi.dof_lists[i].overlapping
-            v[d] .+= cpi.obuffs[i]
+            q[d] .+= cpi.obuffs[i]
         end
     else
         MPI.Send(partition.otempq, comm; dest = 0)
