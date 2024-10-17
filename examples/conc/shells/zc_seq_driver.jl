0a58aed08e7416c83bef164583a0c46d8f536f62
Petr Krysl <pkrysl@ucsd.edu>
2024-10-17 14:57:26 -0700

    clean up

--- examples/conc/shells/zc_seq_driver.jl
+++ examples/conc/shells/zc_seq_driver.jl
@@ -62,172 +62,6 @@ end
 E = 210e9
 nu = 0.3
 L = 10.0
-input = "nle5xf3c.inp"
-
-function _execute(filename, ref, Nc, n1, Np, No, itmax, relrestol, peek, visualize)
-    CTE = 0.0
-    thickness = 0.1
-    
-    tolerance = thickness/1000
-    output = import_ABAQUS(joinpath(dirname(@__FILE__()), input))
-    fens = output["fens"]
-    fes = output["fesets"][1]
-
-    connected = findunconnnodes(fens, fes);
-    fens, new_numbering = compactnodes(fens, connected);
-    fes = renumberconn!(fes, new_numbering);
-
-    for r in 1:ref
-        fens, fes = T3refine(fens, fes)
-    end
-    
-    MR = DeforModelRed3D
-    mater = MatDeforElastIso(MR, 0.0, E, nu, CTE)
-    ocsys = CSys(3, 3, zcant!)
-
-    sfes = FESetShellT3()
-    accepttodelegate(fes, sfes)
-    femm = FEMMShellT3FFModule.make(IntegDomain(fes, TriRule(1), thickness), mater)
-    stiffness = FEMMShellT3FFModule.stiffness
-    associategeometry! = FEMMShellT3FFModule.associategeometry!
-
-    # Construct the requisite fields, geometry and displacement
-    # Initialize configuration variables
-    geom0 = NodalField(fens.xyz)
-    u0 = NodalField(zeros(size(fens.xyz,1), 3))
-    Rfield0 = initial_Rfield(fens)
-    dchi = NodalField(zeros(size(fens.xyz,1), 6))
-
-    # Apply EBC's
-    # plane of symmetry perpendicular to Z
-    l1 = selectnode(fens; box = Float64[0 0 -Inf Inf -Inf Inf], inflate = tolerance)
-    for i in [1,2,3,]
-        setebc!(dchi, l1, true, i)
-    end
-    applyebc!(dchi)
-    numberdofs!(dchi);
-
-    fr = dofrange(dchi, DOF_KIND_FREE)
-    dr = dofrange(dchi, DOF_KIND_DATA)
-    
-    @info("Refinement factor: $(ref)")
-    @info("Number of fine grid partitions: $(Np)")
-    @info("Number of overlaps: $(No)")
-    @info("Number of elements: $(count(fes))")
-    @info("Number of free dofs = $(nfreedofs(dchi))")
-
-    nl = selectnode(fens; box = Float64[10.0 10.0 1.0 1.0 0 0], tolerance = tolerance)
-    loadbdry1 = FESetP1(reshape(nl, 1, 1))
-    lfemm1 = FEMMBase(IntegDomain(loadbdry1, PointRule()))
-    fi1 = ForceIntensity(Float64[0, 0, +0.6e6, 0, 0, 0]);
-    nl = selectnode(fens; box = Float64[10.0 10.0 -1.0 -1.0 0 0], tolerance = tolerance)
-    loadbdry2 = FESetP1(reshape(nl, 1, 1))
-    lfemm2 = FEMMBase(IntegDomain(loadbdry2, PointRule()))
-    fi2 = ForceIntensity(Float64[0, 0, -0.6e6, 0, 0, 0]);
-    F = distribloads(lfemm1, geom0, dchi, fi1, 3) + distribloads(lfemm2, geom0, dchi, fi2, 3);
-    F_f = F[fr]
-
-    associategeometry!(femm, geom0)
-    
-    function make_matrix(fes)
-        femm1 = deepcopy(femm) # for thread safety
-        femm1.integdomain.fes = fes
-        return stiffness(femm1, geom0, u0, Rfield0, dchi);
-    end
-
-    t1 = time()
-    cpi = CoNCPartitioningInfo(fens, fes, Np, No, dchi) 
-    partition_list  = make_partitions(cpi, fes, make_matrix, nothing)
-    partition_sizes = [partition_size(_p) for _p in partition_list]
-    meanps = mean(partition_sizes)
-    @info "Mean fine partition size: $(meanps)"
-    @info "Create partitions ($(round(time() - t1, digits=3)) [s])"
-
-    t1 = time()
-    @info("Number of clusters (requested): $(Nc)")
-    @info("Number of 1D basis functions: $(n1)")
-    nt = n1*(n1+1)/2 
-    (Nc == 0) && (Nc = Int(floor(meanps / nt / ndofs(dchi))))
-    Nepc = count(fes) ÷ Nc
-    (n1 > (Nepc/2)^(1/2)) && @error "Not enough elements per cluster"
-    @info("Number of elements per cluster: $(Nepc)")
-    
-    cpartitioning, Nc = shell_cluster_partitioning(fens, fes, Nepc)
-    @info("Number of clusters (actual): $(Nc)")
-        
-    mor = CoNCData(list -> patch_coordinates(fens.xyz, list), cpartitioning)
-    Phi = transfmatrix(mor, LegendreBasis, n1, dchi)
-    Phi = Phi[fr, :]
-    @info("Size of the reduced problem: $(size(Phi, 2))")
-    @info "Generate clusters ($(round(time() - t1, digits=3)) [s])"
-
-    function peeksolution(iter, x, resnorm)
-        peek && (@info("it $(iter): residual norm =  $(resnorm)"))
-    end
-    
-    t1 = time()
-    Kr_ff = spzeros(size(Phi, 2), size(Phi, 2))
-    for partition in partition_list
-        Kr_ff += (Phi' * partition.nonoverlapping_K * Phi)
-    end
-    Krfactor = lu(Kr_ff)
-    @info "Create global factor ($(round(time() - t1, digits=3)) [s])"
-    
-    t0 = time()
-    M! = preconditioner!(Krfactor, Phi, partition_list)
-    (u_f, stats) = pcg_seq(
-        (q, p) -> partition_multiply!(q, partition_list, p), 
-        F_f, zeros(size(F_f));
-        (M!)=(q, p) -> M!(q, p),
-        peeksolution=peeksolution,
-        itmax=itmax, 
-        # atol=0, rtol=relrestol, normtype = KSP_NORM_NATURAL
-        # atol=relrestol * norm(F_f), rtol=0, normtype = KSP_NORM_NATURAL
-        atol= 0, rtol=relrestol, normtype = KSP_NORM_UNPRECONDITIONED
-        )
-    t1 = time()
-    @info("Number of iterations:  $(stats.niter)")
-    @info "Iterations ($(round(t1 - t0, digits=3)) [s])"
-    stats = (niter = stats.niter, residuals = stats.residuals ./ norm(F_f))
-    data = Dict(
-        "number_nodes" => count(fens),
-        "number_elements" => count(fes),
-        "nfreedofs" => nfreedofs(dchi),
-        "Nc" => Nc,
-        "Np" => Np,
-        "No" => No,
-        "meanps" => meanps,
-        "size_Kr_ff" => size(Krfactor),
-        "stats" => stats,
-        "iteration_time" => t1 - t0,
-    )
-    f = (filename == "" ?
-         "zc-" *
-         "-ref=$(ref)" *
-         "-Nc=$(Nc)" *
-         "-n1=$(n1)" *
-         "-Np=$(Np)" *
-         "-No=$(No)" :
-         filename)
-    @info "Storing data in $(f * ".json")"
-    DataDrop.store_json(f * ".json", data)
-    scattersysvec!(dchi, u_f)
-    
-    if visualize
-        f = (filename == "" ?
-         "zc-" *
-         "-ref=$(ref)" *
-         "-Nc=$(Nc)" *
-         "-n1=$(n1)" *
-         "-Np=$(Np)" *
-         "-No=$(No)" :
-         filename) * "-cg-sol"
-        VTK.vtkexportmesh(f * ".vtk", fens, fes;
-            vectors=[("u", deepcopy(dchi.values[:, 1:3]),)])
-    end
-
-    true
-end
 
 function _execute_alt(filename, ref, Nc, n1, Np, No, itmax, relrestol, peek, visualize)
     CTE = 0.0
@@ -316,9 +150,6 @@ function _execute_alt(filename, ref, Nc, n1, Np, No, itmax, relrestol, peek, vis
 
     associategeometry!(femm, geom0)
 
-    K = stiffness(femm, geom0, u0, Rfield0, dchi)
-    K_ff = K[fr, fr]
-
     @info("Refinement factor: $(ref)")
     @info("Number of fine grid partitions: $(Np)")
     @info("Number of overlaps: $(No)")
@@ -337,7 +168,7 @@ function _execute_alt(filename, ref, Nc, n1, Np, No, itmax, relrestol, peek, vis
     @info "Create CoNCPartitioningInfo ($(round(time() - t1, digits=3)) [s])"
     t2 = time()
     partition_list  = make_partitions(cpi, fes, make_matrix, nothing)
-    @info "make_partitions ($(round(time() - t2, digits=3)) [s])"
+    @info "Make partitions ($(round(time() - t2, digits=3)) [s])"
     partition_sizes = [partition_size(_p) for _p in partition_list]
     meanps = mean(partition_sizes)
     @info "Mean fine partition size: $(meanps)"
@@ -368,29 +199,6 @@ function _execute_alt(filename, ref, Nc, n1, Np, No, itmax, relrestol, peek, vis
     t0 = time()
     M! = TwoLevelPreConditioner(partition_list, Phi)
     @info "Create preconditioner ($(round(time() - t1, digits=3)) [s])"
-    
-    # t0 = time()
-    # function pre(q, p)
-    #     q .= Phi * (M!.Kr_ff_factor \ (Phi' * p))
-    #     for partition in partition_list
-    #         d = partition.entity_list[EXTENDED].global_dofs
-    #         q[d] .+= partition.Kxt_ff_factor \ p[d]
-    #     end
-    #     q
-    # end
-    # b = deepcopy(F_f); x0 = zeros(size(F_f))
-    # (u_f, stats) = pcg_seq(
-    #     (q, p) -> (q .= K_ff * p), 
-    #     b, x0;
-    #     # (M!)=(q, p) -> vec_copyto!(q, p),
-    #     (M!)=(q, p) -> pre(q, p), #(q .= Phi * (Krfactor \ (Phi' * p))), 
-    #     peeksolution=peeksolution,
-    #     itmax=itmax, 
-    #     atol= 0, rtol=relrestol, normtype = KSP_NORM_UNPRECONDITIONED
-    #     )
-    # t1 = time()
-
-    t0 = time()
     x0 = PartitionedVector(Float64, partition_list)
     vec_copyto!(x0, 0.0)
     b = PartitionedVector(Float64, partition_list)
@@ -398,9 +206,7 @@ function _execute_alt(filename, ref, Nc, n1, Np, No, itmax, relrestol, peek, vis
     (u_f, stats) = pcg_seq(
         (q, p) -> aop!(q, p), 
         b, x0;
-        # (M!)=(q, p) -> vec_copyto!(q, p),
         (M!)=(q, p) -> M!(q, p),
-        # (M!)=(q, p) -> (q .= Phi * (Krfactor \ (Phi' * p))), 
         peeksolution=peeksolution,
         itmax=itmax, 
         atol= 0, rtol=relrestol, normtype = KSP_NORM_UNPRECONDITIONED
