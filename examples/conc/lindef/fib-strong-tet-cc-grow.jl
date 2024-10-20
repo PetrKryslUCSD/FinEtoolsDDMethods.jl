3007e825c076b1c8444d1ebeb450fd609e2f0cfe
Petr Krysl <pkrysl@ucsd.edu>
2024-10-20 13:07:37 -0700

    update fiber

--- examples/conc/lindef/fib-strong-tet-cc-grow.jl
+++ examples/conc/lindef/fib-strong-tet-cc-grow.jl
@@ -14,6 +14,6 @@ for Np in [64, 128, 256, 512, ]
     Nc = number_of_clusters(Np)
     filename = "$(prefix)-ref=$ref-Np=$Np-No=$No-Nc=$Nc.json"
     run(`
-    julia --project=. conc/lindef/fib.jl --filename "$filename" --kind $kind --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No --Ef $Ef --nuf $nuf --Em $Em --num $num
+    julia --project=. conc/lindef/fib_seq_driver.jl --filename "$filename" --kind $kind --ref $ref --Nc $Nc --n1 $n1 --Np $Np --No $No --Ef $Ef --nuf $nuf --Em $Em --num $num
     `)
 end
