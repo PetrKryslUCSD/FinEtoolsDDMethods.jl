7c825558a59d5d40c12ff4a519a034a47a0f56e2
Petr Krysl <pkrysl@ucsd.edu>
2024-10-30 09:59:01 -0700

    upd pace_mpi

--- examples/conc/pace_mpi.jl
+++ examples/conc/pace_mpi.jl
@@ -1,23 +1,25 @@
-cmd = `julia --project=. ./conc/heat/Poisson2D_seq_driver.jl`
+mpiexecjl = "mpiexec"
+
+cmd = `$(mpiexecjl) -n 7 julia --project=. ./conc/heat/Poisson2D_mpi_driver.jl`
 run(cmd)
 @info "======================================\nExpected 13 iterations, nearly zero error"
 
-cmd = `julia --project=. ./conc/shells/zc_seq_driver.jl`
+cmd = `$(mpiexecjl) -n 7 julia --project=. ./conc/shells/zc_mpi_driver.jl`
 run(cmd)
 @info "======================================\nExpected 37 iterations"
 
 
-cmd = `julia --project=. ./conc/shells/barrel_seq_driver.jl`
+cmd = `$(mpiexecjl) -n 7 julia --project=. ./conc/shells/barrel_mpi_driver.jl`
 run(cmd)
 @info "======================================\nExpected 68 iterations"
 
 
-cmd = `julia --project=. ./conc/shells/hyp_seq_driver.jl`
+cmd = `$(mpiexecjl) -n 7 julia --project=. ./conc/shells/hyp_mpi_driver.jl`
 run(cmd)
 @info "======================================\nExpected 68 iterations"
 
 
-cmd = `julia --project=. ./conc/lindef/fib_seq_driver.jl`
+cmd = `$(mpiexecjl) -n 7 julia --project=. ./conc/lindef/fib_mpi_driver.jl`
 run(cmd)
 @info "======================================\nExpected 36 iterations"
 
